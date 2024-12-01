36552c1146aae1dafd6e2045a23df393da13d34a
suryaneelankar <suryaneelankar@gmail.com>
2024-12-01 17:49:09 +0530

    release config

--- src/apiconfig.js
+++ src/apiconfig.js
@@ -1,7 +1,7 @@
 // const BASE_URL = "http://192.168.1.6:3000";
-const BASE_URL = "http://192.168.1.5:4000";
+const BASE_URL = "http://65.0.199.60:4000";
 // const BASE_URL = "http://192.168.64.221:3000";
 
-export const LocalHostUrl = '192.168.1.5';
+export const LocalHostUrl = '65.0.199.60';
 
 export default BASE_URL;
