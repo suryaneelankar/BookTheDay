7040f62406c343a546378fbd39232ae68918d84e
suryaneelankar <suryaneelankar@gmail.com>
2025-01-06 20:14:33 +0530

    catering UI Fixes

--- src/components/GradientButton.js
+++ src/components/GradientButton.js
@@ -1,59 +1,61 @@
 import React from "react";
-import {View, Text, TouchableOpacity, StyleSheet, Dimensions} from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
 import LinearGradient from "react-native-linear-gradient";
 import { moderateScale, verticalScale } from "../utils/scalingMetrics";
 import RightArrow from '../assets/svgs/rightSidearrowWhite.svg';
 
-const BookDatesButton = ({ onPress, width, text,padding }) => {
-    return (
-      <TouchableOpacity onPress={onPress} style={styles.container} >
-        <LinearGradient
-          colors={['#D2453B', '#A0153E']}
-          start={{ x: 0, y: 0 }}
-          end={{ x: 1, y: 0 }}
-          style={[styles.buttonView,{width:Dimensions.get('window').width-50, padding:padding ? moderateScale(padding) : 0,alignSelf:'center'}]}
-        > 
-        <View style={{flexDirection:"row",alignItems:"center"}}>
+const BookDatesButton = ({ onPress, width, text, padding, disabled, showIcon = true }) => {
+  return (
+    <TouchableOpacity onPress={onPress} style={styles.container} disabled={disabled}>
+      <LinearGradient
+        colors={['#D2453B', '#A0153E']}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 0 }}
+        style={[styles.buttonView, { width: Dimensions.get('window').width - 50, padding: padding ? moderateScale(padding) : 0, alignSelf: 'center' }]}
+      >
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
           {text === 'Book Dates' ?
-          <Text style={styles.buttonText}>{text}</Text>
-          :
-          <Text style={styles.SubmitbuttonText}>{text}</Text>
-           }
-          <RightArrow style={{marginHorizontal:10,marginTop:2}}/>
-          </View>
-        </LinearGradient>
-      </TouchableOpacity>
-    );
-  };
+            <Text style={styles.buttonText}>{text}</Text>
+            :
+            <Text style={styles.SubmitbuttonText}>{text}</Text>
+          }
+          {showIcon ?
+            <RightArrow style={{ marginHorizontal: 10, marginTop: 2 }} />
+            : null}
+        </View>
+      </LinearGradient>
+    </TouchableOpacity>
+  );
+};
 
-  export default BookDatesButton;
+export default BookDatesButton;
 
-  const styles = StyleSheet.create({
-    container:{
-        backgroundColor:"white",
-        paddingVertical: verticalScale(15),
-        width:Dimensions.get('window').width,
-        alignSelf:"center"
-    },
-    buttonView:{
-        // marginHorizontal:horizontalScale(25),
-        // padding:moderateScale(12),
-        borderRadius:moderateScale(10),
-        alignItems:"center",
- 
-    },
-    buttonText:{
-        color:"#F4F4F6",
-        fontSize:14, 
-        fontWeight:"800",
-        fontFamily: "ManropeRegular",
-        textAlign:"center"   
-   },
-   SubmitbuttonText:{
-    color:"#F4F4F6",
-    fontSize:16, 
-    fontWeight:"600",
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    paddingVertical: verticalScale(15),
+    width: Dimensions.get('window').width,
+    alignSelf: "center"
+  },
+  buttonView: {
+    // marginHorizontal:horizontalScale(25),
+    // padding:moderateScale(12),
+    borderRadius: moderateScale(10),
+    alignItems: "center",
+
+  },
+  buttonText: {
+    color: "#F4F4F6",
+    fontSize: 14,
+    fontWeight: "800",
     fontFamily: "ManropeRegular",
-    textAlign:"center"   
-}
-  })
+    textAlign: "center"
+  },
+  SubmitbuttonText: {
+    color: "#F4F4F6",
+    fontSize: 16,
+    fontWeight: "600",
+    fontFamily: "ManropeRegular",
+    textAlign: "center"
+  }
+})
