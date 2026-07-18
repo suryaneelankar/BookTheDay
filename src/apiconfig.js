26104cad888c884df14dcbe47fcbfc9f3f15d8dd
suryaneelankar <suryaneelankar@gmail.com>
2026-07-18 14:10:54 +0530

    api config url updated

--- src/apiconfig.js
+++ src/apiconfig.js
@@ -1,9 +1,9 @@
 // const BASE_URL = "http://192.168.1.6:3000";
 // const BASE_URL = "http://192.168.134.221:4000";
-const BASE_URL = "http://13.127.254.103:4000"; // Live url
+const BASE_URL = "http://13.201.129.244:4000"; // Live url
 // 65.0.199.60 // live ip address
 // 192.168.0.105
-export const LocalHostUrl = '13.127.254.103';
+export const LocalHostUrl = '13.201.129.244';
 // export const LocalHostUrl = '192.168.134.221';
 
 export default BASE_URL;
